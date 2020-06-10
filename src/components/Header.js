958e1bf2655422d8b58c7c9797bd80aba4e19563
Garrett Gibo <garrett.gibo@gmail.com>
2020-06-10 01:55:53 -0700

    Change navbar to react navbar for stickiness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
+import { Navbar } from 'react-bootstrap'
 import '../App.css';
 
 export class Header extends Component {
     render() {
         return (
-            <div className='picker feature-container' id='header'>
+            <Navbar sticky="top" id='header'>
                 <div className='picker-container flex-100 layout-left'>
                     <h2> Tracking Trending Topics </h2>
                 </div>
-            </div>
+            </Navbar>
         )
     }
 }
