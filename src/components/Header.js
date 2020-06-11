45ac2d1997f690279ce97519129488612acedd2b
Garrett Gibo <garrett.gibo@gmail.com>
2020-06-10 21:14:10 -0700

    Make header sticky with react navbar; add octocat link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
-import { Navbar } from 'react-bootstrap'
+import { Navbar} from 'react-bootstrap'
 import '../App.css';
 
 export class Header extends Component {
     render() {
         return (
             <Navbar sticky="top" id='header'>
-                <div className='picker-container flex-100 layout-left'>
-                    <h2> Tracking Trending Topics </h2>
+                <div className='header flex-100'>
+                    <Navbar.Text bsPrefix='white'>Tracking Trending Topics</Navbar.Text>
+                    <Navbar.Collapse className="justify-content-end">
+                        <a href='https://github.com/garrettgibo/tracking-trending-topics'>
+                        <img
+                            src={require('../images/Octocat.png')}
+                            width="45"
+                            height="45"
+                            className="d-inline-block align-top"
+                            alt="Github Logo"
+                        />
+                        </a>
+                    </Navbar.Collapse>
                 </div>
             </Navbar>
         )
     }
 }
-
 export default Header
